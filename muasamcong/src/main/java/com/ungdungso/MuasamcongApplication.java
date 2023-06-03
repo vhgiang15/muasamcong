1f4d1cea46c54d7d242f2ca006347f5cefc593ea
GIANG DEV <vhgiang15@gmail.com>
2023-06-03 13:27:21 +0700

    first upload

---
+++ muasamcong/src/main/java/com/ungdungso/MuasamcongApplication.java
@@ -0,0 +1,13 @@
+package com.ungdungso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuasamcongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuasamcongApplication.class, args);
+	}
+
+}
---
+++ muasamcong/src/test/java/com/ungdungso/MuasamcongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ungdungso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuasamcongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
