165f444c4e8948ce4594dc61be34a8cd6188d108
GIANG DEV <vhgiang15@gmail.com>
2023-06-08 23:15:24 +0700

    ssl

--- muasamcong/src/main/java/com/ungdungso/controller/HomeController.java
+++ muasamcong/src/main/java/com/ungdungso/controller/HomeController.java
@@ -1,7 +1,14 @@
 package com.ungdungso.controller;
 
 import java.io.IOException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.HostnameVerifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -56,6 +63,25 @@ public class HomeController {
 		  .addHeader("sec-ch-ua-mobile", "?1")
 		  .addHeader("sec-ch-ua-platform", "\"Android\"")
 		  .build();
+		
+	       TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+	            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+	            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+	            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+
+	        } };
+
+	        SSLContext sc = SSLContext.getInstance("SSL");
+	        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+	        // Create all-trusting host name verifier
+	        HostnameVerifier allHostsValid = new HostnameVerifier() {
+	            public boolean verify(String hostname, SSLSession session) { return true; }
+	        };
+	        // Install the all-trusting host verifier
+	        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+		
 		Response response = client.newCall(request).execute();
 		//System.out.println(response.toString());
 		
