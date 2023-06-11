6d2932ea5cdd70e7cd46e1a88ffa8a0bc3970614
GIANG DEV <vhgiang15@gmail.com>
2023-06-11 23:13:52 +0700

    fix SSL

--- muasamcong/src/main/java/com/ungdungso/MuasamcongApplication.java
+++ muasamcong/src/main/java/com/ungdungso/MuasamcongApplication.java
@@ -8,6 +8,9 @@ public class MuasamcongApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuasamcongApplication.class, args);
+		
+		String certificatesTrustStorePath = "C:/Program Files/Java/jdk-17/lib/security/cacerts";
+		System.setProperty("javax.net.ssl.trustStore", certificatesTrustStorePath);
 	}
 
 }
--- muasamcong/src/main/java/com/ungdungso/controller/HomeController.java
+++ muasamcong/src/main/java/com/ungdungso/controller/HomeController.java
@@ -62,26 +62,7 @@ public class HomeController {
 		  .addHeader("sec-ch-ua", "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"")
 		  .addHeader("sec-ch-ua-mobile", "?1")
 		  .addHeader("sec-ch-ua-platform", "\"Android\"")
-		  .build();
-		
-	       TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-	            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
-	            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-	            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-
-	        } };
-
-	        SSLContext sc = SSLContext.getInstance("SSL");
-	        sc.init(null, trustAllCerts, new java.security.SecureRandom());
-	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-	        // Create all-trusting host name verifier
-	        HostnameVerifier allHostsValid = new HostnameVerifier() {
-	            public boolean verify(String hostname, SSLSession session) { return true; }
-	        };
-	        // Install the all-trusting host verifier
-	        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-		
+		  .build();	
 		Response response = client.newCall(request).execute();
 		//System.out.println(response.toString());
 		
