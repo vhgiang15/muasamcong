c5c576f0c5846ac6d83aadb5785663878b86cc63
vhgiang15 <vhgiang15@gmail.com>
2023-06-21 17:36:19 +0700

    change lịch lấy TBMT

--- muasamcong/src/main/java/com/ungdungso/utility/GetBidNotice.java
+++ muasamcong/src/main/java/com/ungdungso/utility/GetBidNotice.java
@@ -21,22 +21,26 @@ import com.ungdungso.repository.DistricRepository;
 
 public class GetBidNotice {
 	private static int totalPage; //số lượng trang trả về khi get API
-	public static List<BidsNotice> getBidsNoticedbyDate( Date dateFrom, Date dateTo, DistricRepository districRepository,BidsNoticeRepostory bidsNoticeRepostory) throws IOException
+	private static int totalElement; //số lượng thông báo mời thầu khi get API
+	
+	
+	public static void getBidsNoticeToDay( ) throws IOException {
+		Date today= new Date();
+		GetBidNotice.getTotalPageandElement(today);
+		
+	}
+	
+	public static void getBidsNoticedbyDate( Date today, DistricRepository districRepository,BidsNoticeRepostory bidsNoticeRepostory) throws IOException
 	{	
 		OkHttpClient client = new OkHttpClient();
 		MediaType mediaType = MediaType.parse("application/json"); 
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		String dateFromString=format.format(dateFrom).toString();
-		String dateToString=format.format(dateTo).toString();		
-		String mediaTypeString="{\"pageNumber\":\"0\",\"query\":[{\"index\":\"es-contractor-selection\",\"matchFields\":[\"notifyNo\",\"bidName\"],\"filters\":[{\"fieldName\":\"publicDate\",\"searchType\":\"range\",\"from\":\"fromDayT00:00:00.000Z\",\"to\":\"toDayT23:59:59.059Z\"},{\"fieldName\":\"type\",\"searchType\":\"in\",\"fieldValues\":[\"es-notify-contractor\"]},{\"fieldName\":\"caseKHKQ\",\"searchType\":\"not_in\",\"fieldValues\":[\"1\"]}]}]}";
-		System.out.println("Chi so indẽ trang");
-		
-		//System.out.println(mediaTypeString.substring(0, 17));
+		String todayString=format.format(today).toString();	
+		String mediaTypeString="{\"pageNumber\":\"0\",\"query\":[{\"index\":\"es-contractor-selection\",\"matchFields\":[\"notifyNo\",\"bidName\"],\"filters\":[{\"fieldName\":\"publicDate\",\"searchType\":\"range\",\"from\":\"todayT00:00:00.000Z\",\"to\":\"todayT23:59:59.059Z\"},{\"fieldName\":\"type\",\"searchType\":\"in\",\"fieldValues\":[\"es-notify-contractor\"]},{\"fieldName\":\"caseKHKQ\",\"searchType\":\"not_in\",\"fieldValues\":[\"1\"]}]}]}";
 		//mediaTypeString=mediaTypeString.replace("{\"pageNumber\":\"0\"","{\"pageNumber\":\"4\"");
 		
 		System.out.println(mediaTypeString);
-		mediaTypeString=mediaTypeString.replace("fromDay", dateFromString);
-		mediaTypeString=mediaTypeString.replace("toDay", dateToString);				
+		mediaTypeString=mediaTypeString.replace("today", todayString);				
 		RequestBody body = RequestBody.create(mediaType,mediaTypeString);				
 		Request request = new Request.Builder()
 		  .url("https://muasamcong.mpi.gov.vn/o/egp-portal-contractor-selection-v2/services/smart/search")
@@ -50,17 +54,7 @@ public class GetBidNotice {
 		  .build();
 		Response response = client.newCall(request).execute();
 		String jsonData = response.body().string();		   
-	    int  tmp= jsonData.lastIndexOf("totalPages"); // tìm kiếm tổng số bản ghi và tổng số trang
-	    int indexSpace=jsonData.indexOf(":", tmp);
-	    int indexComma=jsonData.indexOf(",", tmp);
-	    String totalPageString= jsonData.substring(indexSpace+1, indexComma);
-	    System.out.println("chi so totalPage");
-	    System.out.println(totalPageString);
-		System.out.println(tmp);
-		
-		int tmp1=jsonData.indexOf("totalElements");
-		System.out.println(tmp1);
-	    
+	    int  tmp= jsonData.lastIndexOf("totalPages");  
 	    String temp3=jsonData.substring(8,tmp-2)+"}"; // chi lấy chuổi chứa dữ liệu thông báo mời thầu
 	    JSONObject jobject = new JSONObject(temp3);			    
 	    JSONArray Jarray = jobject.getJSONArray("content");  //2023-06-28T09:00:00
@@ -76,19 +70,55 @@ public class GetBidNotice {
 			if(bidsNoticeRepostory.existsById(bidsNotice.getNotifyNo())) { 
 				System.out.println("Đã tồn tại thông báo mời thầu");
 				System.out.println(bidsNotice.getNotifyNo());
-				continue;}			
-			//bidsNoticeRepostory.save(bidsNotice);
+				continue;} else {
+				bidsNoticeRepostory.save(bidsNotice);
+				}			
 			lisBidsNotices.add(bidsNotice);
 			District district=objectMapper.readValue(tmpString, District.class);
 			if(!districRepository.existsById(district.getDistrictCode())) {
 				districRepository.save(district);
 			}
 		}						
-	return lisBidsNotices;
+	//return lisBidsNotices;
 	}
 	
-	private static String reExcuteString(String objectString) { // tiền xử lý data json
-		
+	public static void getTotalPageandElement(Date today) throws IOException {		
+		OkHttpClient client = new OkHttpClient();
+		MediaType mediaType = MediaType.parse("application/json"); 
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		String todayString=format.format(today).toString();	
+		String mediaTypeString="{\"pageNumber\":\"0\",\"query\":[{\"index\":\"es-contractor-selection\",\"matchFields\":[\"notifyNo\",\"bidName\"],\"filters\":[{\"fieldName\":\"publicDate\",\"searchType\":\"range\",\"from\":\"todayT00:00:00.000Z\",\"to\":\"todayT23:59:59.059Z\"},{\"fieldName\":\"type\",\"searchType\":\"in\",\"fieldValues\":[\"es-notify-contractor\"]},{\"fieldName\":\"caseKHKQ\",\"searchType\":\"not_in\",\"fieldValues\":[\"1\"]}]}]}";
+		mediaTypeString=mediaTypeString.replace("today", todayString);				
+		RequestBody body = RequestBody.create(mediaType,mediaTypeString);				
+		Request request = new Request.Builder()
+		  .url("https://muasamcong.mpi.gov.vn/o/egp-portal-contractor-selection-v2/services/smart/search")
+		  .method("POST", body)
+		  .addHeader("Accept", "application/json, text/plain, */*")
+		  .addHeader("Accept-Language", "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5")
+		  .addHeader("Connection", "keep-alive")
+		  .addHeader("Content-Type", "application/json")
+		  .addHeader("Origin", "https://muasamcong.mpi.gov.vn")
+		  .addHeader("Referer", "https://muasamcong.mpi.gov.vn/web/guest/contractor-selection?render=index")
+		  .build();
+		Response response = client.newCall(request).execute();
+		String jsonData = response.body().string();		   
+	    
+		int  tmp= jsonData.lastIndexOf("totalPages"); // tìm kiếm tổng số bản ghi và tổng số trang
+	    int indexSpace=jsonData.indexOf(":", tmp);
+	    int indexComma=jsonData.indexOf(",", tmp);
+	    totalPage= Integer.valueOf(jsonData.substring(indexSpace+1, indexComma));
+	    System.out.println("chi so totalPage");
+	    System.out.println(totalPage);	    
+		int tmp1=jsonData.indexOf("totalElements");
+	    indexSpace=jsonData.indexOf(":", tmp1);
+	    indexComma=jsonData.indexOf(",", tmp1);
+	    totalElement=Integer.valueOf(jsonData.substring(indexSpace+1, indexComma));
+	    System.out.println("chi so element");
+	    System.out.println(totalElement);
+
+	} 
+	
+	private static String reExcuteString(String objectString) { // tiền xử lý data json, đầu ra là chuối json chỉ chứa dữ liệu thông báo mời thầu	
 		int tmp=objectString.indexOf("\"pvccNew\":");   // xoá key pvccNew
 		int tmp2= objectString.indexOf("],", tmp);
 		String firString=objectString.substring(0, tmp);
--- muasamcong/src/main/java/com/ungdungso/utility/ScheduleTask.java
+++ muasamcong/src/main/java/com/ungdungso/utility/ScheduleTask.java
@@ -1,13 +1,10 @@
 package com.ungdungso.utility;
 import java.io.IOException;
 import java.util.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import com.ungdungso.model.BidsNotice;
 import com.ungdungso.repository.BidsNoticeRepostory;
 import com.ungdungso.repository.DistricRepository;
 
@@ -18,13 +15,10 @@ public class ScheduleTask {
 	private BidsNoticeRepostory bidsNoticeRepostory;
 	@Autowired
 	private DistricRepository districRepository;
-
 	@Scheduled(cron = "5 * * * * ?") 
 	public void scheduleGetBidNotice() throws IOException {
-		Date toDay= new Date();
-		List<BidsNotice> listBid=GetBidNotice.getBidsNoticedbyDate(toDay, toDay,districRepository, bidsNoticeRepostory);
-		for (BidsNotice bidsNotice : listBid) {			
-			bidsNoticeRepostory.save(bidsNotice);
-		}				
+		Date today= new Date();
+		GetBidNotice.getTotalPageandElement(today);
+		GetBidNotice.getBidsNoticedbyDate(today,districRepository, bidsNoticeRepostory);			
 }
 }
